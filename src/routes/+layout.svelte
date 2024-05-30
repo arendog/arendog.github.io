a3814e3b7038defd2b32499ebc054a520c0462a0
arendog <alex@arennie.co.uk>
2024-05-30 16:45:40 +0100

    small reduced margin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
 	<Sidenav />
 </div>
 
-<div class="mx-10 mt-4 md:ml-60 md:mr-0 md:mt-10 md:w-[42rem] lg:ml-80">
+<div class="mx-6 mt-4 md:ml-60 md:mr-0 md:mt-10 md:w-[42rem] lg:ml-80">
 	<slot />
 </div>
 
