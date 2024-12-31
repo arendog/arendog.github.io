665d14044ed2857e551070311794a608159fa0e3
arendog <alex@arennie.co.uk>
2024-12-31 14:08:15 +0000

    Added 404 for disabled pages

--- src/routes/works/[slug]/+page.ts
+++ src/routes/works/[slug]/+page.ts
@@ -4,13 +4,17 @@ export async function load({ params }) {
 	try {
 		const work = await import(`$lib/data/works/${params.slug}.svx`);
 
+		if (work.metadata.page) {
 		return {
 			content: work.default,
 			meta: work.metadata
 		};
+		} else {
+			throw error(404, 'Page not found');
+		}
 	} catch (e) {
 		console.log(e);
-		throw error(404, 'Slug is invalid');
+		throw error(404, 'Page not found');
 	}
 }
 
