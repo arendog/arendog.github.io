bba11352050d75f16d8ae98c863cbecaa446ed00
arendog <alex@arennie.co.uk>
2025-09-27 00:15:14 +0100

    Improve volume slider styling

--- src/components/Slider.svelte
+++ src/components/Slider.svelte
@@ -16,5 +16,5 @@
 	{#if label}
 		<label class="text-sm text-darkgrey" for={id}>{label}</label>
 	{/if}
-	<input class="w-20 border-primary" type="range" {id} {name} {min} {max} bind:value {step} />
+	<input class="w-20 border-primary" type="range" {id} {name} {min} {max} bind:value {step}  style="--val: {value * 100}%;" />
 </div>
