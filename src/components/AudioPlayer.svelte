e221a20046ec3ab95dea79ead06ad37d065a55bd
arendog <alex@arennie.co.uk>
2025-09-26 17:55:30 +0100

    fixed seconds timer in audio player

--- src/components/AudioPlayer.svelte
+++ src/components/AudioPlayer.svelte
@@ -57,7 +57,7 @@
 
 	const formatTime = (seconds: number) => {
 		const minutes = Math.floor(seconds / 60);
-		const secondsRemainder = Math.round(seconds) % 60;
+		const secondsRemainder = Math.floor(seconds) % 60;
 		const paddedSeconds = `0${secondsRemainder}`.slice(-2);
 		return `${minutes}:${paddedSeconds}`;
 	};
