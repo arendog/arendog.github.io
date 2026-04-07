73f387b153935da0c7e5909ce5181384cc5e2317
arendog <alex@arennie.co.uk>
2026-04-07 23:52:44 +0100

    Prerender slugs

--- src/routes/works/[slug]/+page.ts
+++ src/routes/works/[slug]/+page.ts
@@ -1,19 +1,45 @@
 import { error } from '@sveltejs/kit';
 
-export async function load({ params }) {
-	try {
-		const work = await import(`$lib/data/works/${params.slug}.svx`);
+type WorkModule = {
+	default: any;
+	metadata: {
+		page?: boolean;
+		[key: string]: any;
+	};
+};
 
-		if (work.metadata.page) {
-			return {
-				content: work.default,
-				meta: work.metadata
-			};
-		} else {
-			throw error(404, 'Page not found');
-		}
-	} catch (e) {
-		console.log(e);
+// strongly type the glob
+const modules = import.meta.glob<WorkModule>(
+	'/src/lib/data/works/*.svx'
+);
+
+export const prerender = true;
+
+// generate slugs
+export function entries() {
+	return Object.keys(modules).map((path) => {
+		const slug = path.split('/').pop()!.replace('.svx', '');
+		return { slug };
+	});
+}
+
+export async function load({ params }) {
+	const path = `/src/lib/data/works/${params.slug}.svx`;
+
+	const loader = modules[path];
+
+	if (!loader) {
 		throw error(404, 'Page not found');
 	}
+
+	const work = await loader();
+
+	if (work.metadata?.page) {
+		return {
+			content: work.default,
+			meta: work.metadata
+		};
+	}
+
+	throw error(404, 'Page not found');
 }
--- src/routes/writings/[slug]/+page.ts
+++ src/routes/writings/[slug]/+page.ts
@@ -1,19 +1,45 @@
 import { error } from '@sveltejs/kit';
 
-export async function load({ params }) {
-	try {
-		const writing = await import(`$lib/data/writings/${params.slug}.svx`);
+type WorkModule = {
+	default: any;
+	metadata: {
+		page?: boolean;
+		[key: string]: any;
+	};
+};
 
-		if (writing.metadata.page) {
-			return {
-				content: writing.default,
-				meta: writing.metadata
-			};
-		} else {
-			throw error(404, 'Page not found');
-		}
-	} catch (e) {
-		console.log(e);
+// strongly type the glob
+const modules = import.meta.glob<WorkModule>(
+	'/src/lib/data/writings/*.svx'
+);
+
+export const prerender = true;
+
+// generate slugs
+export function entries() {
+	return Object.keys(modules).map((path) => {
+		const slug = path.split('/').pop()!.replace('.svx', '');
+		return { slug };
+	});
+}
+
+export async function load({ params }) {
+	const path = `/src/lib/data/writings/${params.slug}.svx`;
+
+	const loader = modules[path];
+
+	if (!loader) {
 		throw error(404, 'Page not found');
 	}
+
+	const writing = await loader();
+
+	if (writing.metadata?.page) {
+		return {
+			content: writing.default,
+			meta: writing.metadata
+		};
+	}
+
+	throw error(404, 'Page not found');
 }
